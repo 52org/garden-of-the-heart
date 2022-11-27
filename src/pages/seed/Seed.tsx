cb03036059103de4506c8fffc0d9ab835c44f681
JuhyunLim <87933367+userJu@users.noreply.github.com>
2022-11-27 23:03:40 +0900

    feat: 씨앗 보내기 상세페이지 출력
    주석: 차후 react-router 이용 시 사용할 부분

--- src/pages/seed/Seed.tsx
+++ src/pages/seed/Seed.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
-
 const Seed: React.FC = () => {
-  return <div></div>;
+  // const location = useLocation()
+  // const {plantName, imgURL} = location.state.seed
+  return (
+    <div>
+      <img src='imgURL' alt='' />
+      <title>plantName</title>
+      <span>물주기 횟수 : growingPeriod</span>
+      <p>description</p>
+
+      <button>plantName심으러가기</button>
+    </div>
+  );
 };
 
 export default Seed;
