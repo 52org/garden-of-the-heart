a232b149136ce13a57d70478790fe2412385edcd
msdio <rlagofka1@gmail.com>
2022-11-27 22:17:09 +0900

    add: 받은 편지 페이지

--- src/pages/message/Message.tsx
+++ src/pages/message/Message.tsx
@@ -1,9 +1,28 @@
 import React from "react";
 
-const Message: React.FC = () => {
-  return (
-    <div>
+interface Letter {
+  plantName: string;
+  author: string;
+  message: string;
+  createDate: string;
+}
 
+const letterData: Letter = {
+  plantName: "토마토",
+  author: "haru",
+  message: "안녕! 안녕! 안녕! 안녕! \n 안녕! 안녕! 안녕! 안녕! 안녕! \n 안녕! 안녕! 안녕! 안녕! 안녕! 안녕! 안녕! 안녕! 안녕! 안녕! 안녕! 안녕! 안녕! 안녕! 안녕!",
+  createDate: "2022-11-27"
+}
+
+const Message: React.FC = () => {
+  
+  return (
+    <div className="border-2 rounded w-full h-full relative p-8">
+      <p className="whitespace-pre-line">{letterData.message}</p>
+      <div className="absolute right-5 bottom-5 text-right">
+        <p className="mb-2">{letterData.createDate}</p>
+        <p>{letterData.author}</p>
+      </div>
     </div>
   );
 }
