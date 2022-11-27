b4125f4ba58ccde6ac89ccd0d7f965b3be7bf8b8
JuhyunLim <87933367+userJu@users.noreply.github.com>
2022-11-27 23:04:33 +0900

    Feat: 씨앗 주머니에서 씨앗 클릭 시 이동 로직 작성
    주석: 차후 react-router 이용 시 사용할 부분 작성

--- src/pages/seed-bag/SeedBag.tsx
+++ src/pages/seed-bag/SeedBag.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import type { Seed } from 'seed';
 import { seedList } from 'seed';
 
 const SeedBag: React.FC = () => {
+  const onSeedboxClick = (e: React.MouseEvent<HTMLDivElement>, seed: Seed) => {
+    console.log(seed);
+    // useNavigate('이동 경로', {state:seed})
+  };
   return (
     <div>
       {seedList.map((seed, index) => (
-        <div key={seed.plantName}>
+        <div key={seed.plantName} onClick={(e) => onSeedboxClick(e, seed)}>
           <img src={seed.imgUrl} alt={seed.plantName} />
           <div>
             <span>{seed.plantName}</span>
