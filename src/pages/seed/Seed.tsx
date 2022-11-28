c461924bef9343456e03d0ee8020e40aa6e7eaa8
JuhyunLim <87933367+userJu@users.noreply.github.com>
2022-11-28 22:04:20 +0900

    feat: 심으러 가기 클릭 시 messageform으로 라우팅

--- src/pages/seed/Seed.tsx
+++ src/pages/seed/Seed.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { SeedTable } from 'seed';
 
 const Seed: React.FC = () => {
+  const navigate = useNavigate();
   const location = useLocation();
   const { seedName } = location.state;
   const seed = SeedTable[seedName];
   const { imgUrl, plantName, growingPeriod, description } = seed;
+
+  const routeToMessageForm = () => {
+    navigate('/messageform', { state: { seedName } });
+  };
   return (
     <div>
       <img src={imgUrl} alt='' />
@@ -14,7 +19,9 @@ const Seed: React.FC = () => {
       <span>물주기 횟수 : {growingPeriod}</span>
       <p>{description.skill}</p>
       <p>{description.description}</p>
-      <button>{plantName} 심으러가기</button>
+      <p>
+        {plantName} <button onClick={routeToMessageForm}>심으러가기</button>
+      </p>
     </div>
   );
 };
