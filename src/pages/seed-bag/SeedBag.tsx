3a75e2df0df83331d166ecb8e7c6b52f1fb8a379
jihyunlee <jihyunlee.me@gmail.com>
2022-11-29 04:00:37 +0900

    fix: seedbag에서 배경화면이 스크롤 아래로는 차지 않는 문제

--- src/pages/seed-bag/SeedBag.tsx
+++ src/pages/seed-bag/SeedBag.tsx
@@ -8,18 +8,19 @@ const SeedBag: React.FC = () => {
   const onSeedboxClick = (e: React.MouseEvent<HTMLDivElement>, seedName: string) => {
     navigate(`/seed/${seedName}`, { state: { seedName } });
   };
+
   return (
-    <div className='bg-bgColor text-center mt-5 mb-3'>
+    <div className='text-center mt-5 mb-3 bg-bgColor-100'>
       <span className='font-extrabold font-xxl'>씨앗 주머니</span>
       <div className='flex flex-wrap justify-center'>
         {seedList.map((seed, index) => (
           <div
-            className=' inline-block w-4/12 m-5 border'
+            className='inline-block w-4/12 m-5 border'
             key={seed.plantName}
             onClick={(e) => onSeedboxClick(e, seed.plantName)}
           >
             <img className='border m-auto' src={seed.imgUrl} alt={seed.plantName} />
-            <div className=' flex justify-between m-auto'>
+            <div className='flex justify-between m-auto'>
               <span className='ml-2'>{seed.plantName}</span>
               <span className='mr-2'>{seed.growingPeriod}</span>
             </div>
