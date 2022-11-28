8bdd63d908c836268c44f6f68ce3b7b66343cf46
jihyunlee <jihyunlee.me@gmail.com>
2022-11-28 23:08:46 +0900

    feat: letter export용 index파일 생성

---
+++ src/assets/image/letter/index.ts
@@ -0,0 +1,12 @@
+import apple from './apple._letter.png';
+import banana from './banana_letter.png';
+import carrot from './carrot_letter.png';
+import ginseng from './ginseng_letter.png';
+import melon from './melon_letter.png';
+import potato from './potato_letter.png';
+import rose from './rose_letter.png';
+import sunflower from './sunflower_letter.png';
+import sweetPotato from './sweetpotato_letter.png';
+import tomato from './tomato_letter.png';
+
+export { apple, banana, carrot, ginseng, melon, potato, rose, sunflower, sweetPotato, tomato };
