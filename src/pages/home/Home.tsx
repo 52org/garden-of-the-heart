efe728197704d73756bb92edb0501275c8535885
JuhyunLim <87933367+userJu@users.noreply.github.com>
2022-11-29 03:16:44 +0900

    feat: home css 작업

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,14 +1,28 @@
+import { main_image } from 'assets/image';
 import React from 'react';
 
 import CreateOwnerBox from './components/CreateOwnerBox';
 
 const Home: React.FC = () => {
   return (
-    <div className='flex items-center justify-center w-full h-full'>
+    <div className='flex items-center justify-center w-full h-full text-center'>
       <div className='w-fit h-fit'>
-        <h1>마음의 정원</h1>
+        <h1 className='font-extrabold text-3xl mt-5 mb-7'>마음의 정원</h1>
         <p>소개</p>
-        <div className='w-full h-[300px] bg-sky-200'>이미지(작물 이미지 / 텃밭 이미지)</div>
+        <div className='w-full h-[300px] bg-transparent relative'>
+          <div
+            className='w-3/12 h-3/12 bg-sky-100'
+            style={{
+              boxShadow: '0px 0px 50px 50px rgba(176,255,237,0.5)',
+              position: 'absolute',
+              transform: 'translate(-50%,-50%)',
+              top: '50%',
+              left: '50%',
+              borderRadius: '50%',
+            }}
+          ></div>
+          <img className='h-full w-auto' src={main_image} alt='' />
+        </div>
         <CreateOwnerBox />
       </div>
     </div>
