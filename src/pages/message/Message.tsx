a8055cc866f95bbb148a17e1ed541d07415f40e4
Leejin-Yang <ejin1996@gmail.com>
2022-11-29 01:06:45 +0900

    feat: 메세지 Letter 컴포넌트 적용

--- src/pages/message/Message.tsx
+++ src/pages/message/Message.tsx
@@ -1,12 +1,18 @@
+import Letter from 'components/letter';
 import dayjs from 'dayjs';
 import useGetLetterDetail from 'hooks/useGetLetterDetail';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
+import LetterContent from './components/LetterContent';
+
 const Message: React.FC = () => {
   const { letterId } = useParams();
-
   const { isLoading, data } = useGetLetterDetail(letterId as string);
+
+  if (!data) {
+    return null;
+  }
 
   return (
     <>
@@ -14,18 +20,20 @@ const Message: React.FC = () => {
         <div className='flex justify-center items-center text-xl'>로딩중...</div>
       ) : (
         <div className='border-2 rounded w-full h-full relative p-8'>
-          <p className='whitespace-pre-line'>{data?.message}</p>
-          <div className='absolute right-5 bottom-5 text-right'>
-            <p className='mb-2'>{formatDate(String(data?.createDate))}</p>
-            <p>{data?.author}</p>
-          </div>
+          <Letter receiver=''>
+            <LetterContent
+              date={formatDate(data?.createDate)}
+              author={data?.author}
+              message={data?.message}
+            />
+          </Letter>
         </div>
       )}
     </>
   );
 };
 
-const formatDate = (createDate: string) => {
+const formatDate = (createDate: Date) => {
   return dayjs(createDate, 'YYYY-MM-DD').format('YY.MM.DD');
 };
 
