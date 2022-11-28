0ec005ecb1f5ec2b7fd91bd8bf298eace1d4a2bb
jihyunlee <jihyunlee.me@gmail.com>
2022-11-28 23:11:41 +0900

    fix: no-empty-function error 임시 fix

--- src/pages/message-form/MessageForm.tsx
+++ src/pages/message-form/MessageForm.tsx
@@ -1,16 +1,12 @@
 import Letter from 'components/letter';
 import React from 'react';
 
-import LetterForm from './components/LetterForm';
-
 const MessageForm: React.FC = () => {
   // redux 스토어 저장 로직 -> onSubmit prop 넘겨주기
 
   return (
     <div>
-      <Letter receiver='준'>
-        <LetterForm onSubmit={() => {}} />
-      </Letter>
+      <Letter receiver='준'>{/* <LetterForm onSubmit={() => {}} /> */}</Letter>
     </div>
   );
 };
