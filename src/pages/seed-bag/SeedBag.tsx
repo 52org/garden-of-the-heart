fede8f16f01b28c4e8a102885d48e5c8c495b3cf
JuhyunLim <87933367+userJu@users.noreply.github.com>
2022-11-29 02:06:39 +0900

    feat: 씨앗 주머니 기초적인 디자인 정렬

--- src/pages/seed-bag/SeedBag.tsx
+++ src/pages/seed-bag/SeedBag.tsx
@@ -9,16 +9,23 @@ const SeedBag: React.FC = () => {
     navigate(`/guest/seed/${seedName}`, { state: { seedName } });
   };
   return (
-    <div>
-      {seedList.map((seed, index) => (
-        <div key={seed.plantName} onClick={(e) => onSeedboxClick(e, seed.plantName)}>
-          <img src={seed.imgUrl} alt={seed.plantName} />
-          <div>
-            <span>{seed.plantName}</span>
-            <span>{seed.growingPeriod}</span>
+    <div className='bg-bgColor text-center mt-5 mb-3'>
+      <span className='font-extrabold font-xxl'>씨앗 주머니</span>
+      <div className='flex flex-wrap justify-center'>
+        {seedList.map((seed, index) => (
+          <div
+            className=' inline-block w-4/12 m-5 border'
+            key={seed.plantName}
+            onClick={(e) => onSeedboxClick(e, seed.plantName)}
+          >
+            <img className='border m-auto' src={seed.imgUrl} alt={seed.plantName} />
+            <div className=' flex justify-between m-auto'>
+              <span className='ml-2'>{seed.plantName}</span>
+              <span className='mr-2'>{seed.growingPeriod}</span>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
