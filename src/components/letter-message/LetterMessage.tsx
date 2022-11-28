fc64e643b26789bafbb3887ada26d0c3a4ba5397
msdio <rlagofka1@gmail.com>
2022-11-28 21:07:39 +0900

    fix: 조건 수정

--- src/components/letter-message/LetterMessage.tsx
+++ src/components/letter-message/LetterMessage.tsx
@@ -9,12 +9,12 @@ const LetterMessage: React.FC<LetterMessageProps> = ({ isWritable, content }) =>
   return (
     <div className='border-2 rounded w-full h-full relative p-8'>
       {isWritable ? (
-        <p className='whitespace-pre-line'>{content}</p>
-      ) : (
         <textarea
           className='w-full h-full p-4 border-zinc-300 rounded border-solid border-2 focus:outline-zinc-500 resize-none'
           placeholder='편지 내용을 입력해주세요!'
         ></textarea>
+      ) : (
+        <p className='whitespace-pre-line'>{content}</p>
       )}
     </div>
   );
