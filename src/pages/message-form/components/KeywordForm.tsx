9000cc9324e299b96c9c130d5864a490e279a2ad
Leejin-Yang <ejin1996@gmail.com>
2022-11-29 03:32:44 +0900

    feat: 키워드 폼 스타일

--- src/pages/message-form/components/KeywordForm.tsx
+++ src/pages/message-form/components/KeywordForm.tsx
@@ -19,18 +19,21 @@ const KeywordForm: React.FC<KeywordFormProps> = ({ growingPeriod, isMaxKeywords,
     const enteredKeyword = keywordInput.current?.value;
 
     if (!enteredKeyword) return;
+    if (enteredKeyword.length > 10) return;
 
     setKeyword(enteredKeyword);
   };
 
   return (
-    <form onSubmit={onSubmitKeyword} id='keyword-form'>
-      <label htmlFor='keyword'>잎새 * 자라는 동안 보여질 키워드입니다 (최대 10자)</label>
+    <form onSubmit={onSubmitKeyword} id='keyword-form' className='relative flex flex-col'>
+      <label htmlFor='keyword' className='text-xs'>
+        <span className='text-base'>잎새</span> * 자라는 동안 보여질 키워드입니다. (최대 10자)
+      </label>
       <input
         type='text'
         placeholder={`최대 ${growingPeriod - 1}개`}
         id='keyword'
-        className='border-zinc-300 rounded border-solid border-2 py-2 pl-1 pr-5 text-left'
+        className='border-borderColor-100 rounded border-solid border-2 py-2 pl-1 pr-5 text-left'
         ref={keywordInput}
         form='keyword-form'
       />
--- src/pages/message-form/components/KeywordList.tsx
+++ src/pages/message-form/components/KeywordList.tsx
@@ -16,10 +16,15 @@ const KeywordList: React.FC<KeywordListProps> = ({ keyWords, deleteKeyword }) =>
   };
 
   return (
-    <ul>
+    <ul className='flex flex-row [&>li+li]:ml-3'>
       {keyWords?.map((keyword, index) => (
-        <li key={`${keyword}-${index}`}>
-          <button type='button' onClick={onKeywordClick} data-keyword={keyword}>
+        <li key={`${keyword}-${index}`} className='rounded-lg border-2 bg-btnColor-100'>
+          <button
+            type='button'
+            onClick={onKeywordClick}
+            data-keyword={keyword}
+            className='px-3 py-1'
+          >
             {keyword}
           </button>
         </li>
