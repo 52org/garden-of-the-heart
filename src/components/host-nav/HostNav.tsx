ee5d2f71cdce88db5866f47fe05e746a267491c2
Ji Hee Han <g2hhh2ee@gmail.com>
2022-11-29 15:10:45 +0900

    style: change gnb images

--- src/components/host-nav/HostNav.tsx
+++ src/components/host-nav/HostNav.tsx
@@ -13,7 +13,7 @@ const HostNav: React.FC = () => {
       <div className='container flex flex-wrap items-center justify-between h-20 mx-auto'>
         <div className='flex items-center justify-center w-20 h-20 ml-6 overflow-hidden bg-gray-200 rounded-full'>
           <Link to='/messagebox'>
-            <img className='h-full sm:h-9' src={NavImage.seedbag_gnb} alt='편지함' />
+            <img className='h-full sm:h-9' src={NavImage.letter_gnb} alt='편지함' />
           </Link>
         </div>
         <div className='flex items-center justify-center w-20 h-20 mr-6 overflow-hidden bg-gray-200 rounded-full'>
