79d1bde1f8e634d1c1e282f4a45be1c601aca055
Leejin-Yang <ejin1996@gmail.com>
2022-11-29 15:06:29 +0900

    feat: 작물 성장 이미지 유틸

---
+++ src/assets/image/plant-detail/index.ts
@@ -0,0 +1,4 @@
+import seedGround from './seed_ground.png';
+import sprout from './sprout.png';
+
+export { seedGround, sprout };
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,27 +1,15 @@
-import * as PlantImage from 'assets/image/plants';
+import * as PlantDetailImage from 'assets/image/plant-detail';
 import { SeedTable } from 'seed';
-
-interface ImageTable {
-  [key: string]: string;
-}
-
-const plantImageTable = {
-  사과: PlantImage.apple,
-  바나나: PlantImage.banana,
-  당근: PlantImage.carrot,
-  산삼: PlantImage.ginseng,
-  메론: PlantImage.melon,
-  감자: PlantImage.potato,
-  장미: PlantImage.rose,
-  해바라기: PlantImage.sunflower,
-  고구마: PlantImage.sweetPotato,
-  토마토: PlantImage.tomato,
-};
-
-const createGetImage = (imageTable: ImageTable) => (key: string) => imageTable[key] ?? 'default';
-
-export const getPlantImage = createGetImage(plantImageTable);
 
 export const getSeedInfo = (plantName: string) => SeedTable[plantName];
 
+export const getPlantImage = (plantName: string) => getSeedInfo(plantName)?.imgUrl;
+
 export const getGrowingPeriod = (plantName: string) => getSeedInfo(plantName)?.growingPeriod;
+
+export const getPlantDetailImage = (wateringCount: number, plantName: string) => {
+  if (wateringCount === getGrowingPeriod(plantName)) return getPlantImage(plantName);
+  if (wateringCount === 0) return PlantDetailImage.seedGround;
+
+  return PlantDetailImage.sprout;
+};
