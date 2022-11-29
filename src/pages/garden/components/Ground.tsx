95b0ead72591fede7396d78cd11b8b40cf448b00
Ji Hee Han <g2hhh2ee@gmail.com>
2022-11-29 20:50:45 +0900

    style: add base ground

--- src/pages/garden/components/Ground.tsx
+++ src/pages/garden/components/Ground.tsx
@@ -1,3 +1,4 @@
+import { ground_base } from 'assets/image/garden';
 import type { Plant } from 'entities/plant';
 import React from 'react';
 
@@ -11,6 +12,7 @@ interface GroundProps {
 const Ground: React.FC<GroundProps> = ({ plantList, isHost }) => {
   return (
     <div className='w-[360px] h-[360px] flex-wrap bg-groundColor-100 flex justify-start overflow-hidden'>
+      <img className='w-full h-full' src={ground_base} alt='땅'></img>
       {plantList.map((plant) => (
         <Tile key={plant.letterId} plant={plant} isHost={isHost} />
       ))}
