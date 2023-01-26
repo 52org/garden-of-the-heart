8d7adca078fd2d435d77fe200063f32c666896ba
Ji Hee Han <g2hhh2ee@gmail.com>
2023-01-26 21:49:02 +0900

    feat: add shaking effect on seed

--- src/pages/garden/components/Tile.tsx
+++ src/pages/garden/components/Tile.tsx
@@ -1,3 +1,5 @@
+import '../assets/Tile.css';
+
 import type { Plant } from 'entities/plant';
 import React, { useCallback, useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -19,7 +21,7 @@ const Tile: React.FC<TileProps> = ({ plant, isHost }) => {
     if (isHost) {
       navigate(`/plant/${plant.letterId}`);
     } else {
-      toast.info('텃밭의 주인이 물을 주면서 당신의 편지를 기다릴거에요. 😊');
+      toast.info('텃밭의 주인이 물을 주면서 당신의 편지를 기다릴 거예요. 😊');
     }
   }, [plant, isHost, navigate]);
 
@@ -29,7 +31,7 @@ const Tile: React.FC<TileProps> = ({ plant, isHost }) => {
       className='m-0 p-0 block w-[120px] h-[120px] bg-black bg-opacity-20'
     >
       <div className='relative overflow-hidden'>
-        <img src={imgUrl} alt='직물' />
+        <img className='shake' src={imgUrl} alt='작물' />
       </div>
     </button>
   );
