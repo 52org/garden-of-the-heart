7f4509dcc0f3f0e83532ef31a0c078b4f299e34d
jihyunlee <jihyunlee.me@gmail.com>
2023-01-30 19:39:15 +0900

    feat: mock data respose 추가

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -3,7 +3,11 @@ import { rest } from 'msw';
 const handlers = [
   rest.post('/login', (req, res, ctx) => {
     localStorage.setItem('access-token', 'test-token-by-msw');
-    return res(ctx.status(200));
+    return res(ctx.status(200),
+    ctx.json({
+      message:'로그인 성공',
+      data: 'hello mocking data'
+    }));
   }),
   rest.get('/messagebox', (req, res, ctx) => {
     const isAuthenticated = localStorage.getItem('access-token');
@@ -19,6 +23,23 @@ const handlers = [
       ctx.status(200),
       ctx.json({
         username: 'user_john',
+      }),
+    );
+  }),
+  rest.get('/getGarden:/uuid', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        message: '성공',
+        data: {
+          name: '고구마',
+          plantList : {
+            letterId: '1',
+            plantName: '사과',
+            wateringCount: 0,
+            isWatered: false,
+          }
+        }
       }),
     );
   }),
