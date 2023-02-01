abd6a9629800ee94a5d3133764b129a459496b5d
Juhyun <boochickenood@gmail.com>
2023-02-01 21:07:47 +0900

    feat: Add navigation feature on title(header)
    header에 뒤로가기와 접속해있는 텃밭 페이지로 바로 이동할 수 있는 네비게이션 기능을 구현했습니다.
    2차 스프린트에서 해결하고자 했던 이슈였던 기존 네비게이션 바 위치 문제를 해결할 수 있습니다

--- src/pages/garden/components/Title.tsx
+++ src/pages/garden/components/Title.tsx
@@ -1,17 +1,36 @@
 import { useAppSelector } from 'hooks';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const Title: React.FC = () => {
-  const { name } = useAppSelector((state) => state.base);
+interface TitleProps {
+  isHost: boolean;
+}
+
+const Title: React.FC<TitleProps> = ({ isHost }) => {
+  const { name, uuid } = useAppSelector((state) => state.base);
+  const navigate = useNavigate();
+
+  const clickGoBackButton = () => {
+    navigate(-1);
+  };
+
+  const clickGoGardenButton = () => {
+    navigate(`/${isHost ? 'host' : 'guest'}/garden/${uuid}`);
+  };
 
   return (
     <h1 className='absolute top-0 left-0 z-10 w-full h-8 px-8 overflow-hidden rounded'>
       <div className='absolute top-0 left-0 z-10 w-full h-8 px-4 bg-white opacity-60 blur-md'>
         <div className='w-full'></div>
       </div>
-      <span className='relative z-30 block text-3xl leading-8 text-center'>
-        {name ?? ''}의 텃밭
-      </span>
+      <div className='relative z-30 block text-3xl leading-8 text-center'>
+        <button onClick={clickGoBackButton} className=' absolute left-1' title='뒤로가기'>
+          {'<'}
+        </button>
+        <button onClick={clickGoGardenButton} title={`${name ?? ''}의 텃밭으로 가기`}>
+          {name ?? ''}의 텃밭
+        </button>
+      </div>
       <div
         style={{
           marginTop: '1.25rem',
