866b7eadff7aaa0f60e396c8d098862e134cbb99
Jihyun Lee <jihyunlee.me@gmail.com>
2023-02-12 03:59:52 +0900

    feat: BigButton disabled 추가

--- src/components/bigButton/BigButton.tsx
+++ src/components/bigButton/BigButton.tsx
@@ -3,13 +3,15 @@ import React from 'react';
 interface BigButtonProps {
   handleClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
   buttonName: string;
+  disabled?: boolean;
 }
 
-const BigButton: React.FC<BigButtonProps> = ({ handleClick, buttonName }) => {
+const BigButton: React.FC<BigButtonProps> = ({ handleClick, buttonName, disabled = false }) => {
   return (
     <button
-      className=' bg-groundColor-100 text-textColor-200 outline-none py-3 px-9 border-2 border-btnColor-100 border-dotted rounded-2xl'
+      className='bg-groundColor-100 text-textColor-200 outline-none py-3 px-9 border-2 border-btnColor-100 border-dotted rounded-2xl'
       onClick={handleClick}
+      disabled={disabled}
     >
       <p className='text-xl font-bold'>{buttonName}</p>
     </button>
