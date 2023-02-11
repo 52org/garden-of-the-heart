7ebf4a44c4e29e9ad5f3fd9118d15d18677b1713
Jihyun Lee <jihyunlee.me@gmail.com>
2023-02-12 04:00:24 +0900

    feat: BigInput props 추가

--- src/components/bigInput/BigInput.tsx
+++ src/components/bigInput/BigInput.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 
+type RefProps = React.Ref<HTMLInputElement>;
 interface BigInputProps {
   handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   placeholder: string;
+  name?: string;
 }
 
-const BigInput: React.FC<BigInputProps> = ({ handleChange, placeholder }) => {
+const BigInput: React.FC<BigInputProps> = ({ handleChange, placeholder, name}) => {
   return (
     <input
       className='w-8/12 border-b-2 border-b-groundColor-200 mb-7 text-lg outline-none bg-transparent text-center'
       type='text'
+      name={name}
       onChange={handleChange}
       placeholder={placeholder}
+      // ref={ref}
     />
   );
 };
